ce2c63aba3d928f0e83995e42101908c2a715bce
pujatiwa <pujat4582@gmail.com>
2024-09-09 00:47:18 +0530

    About file is added

--- Lib-Sys/src/components/Loader/Loader.jsx
+++ Lib-Sys/src/components/Loader/Loader.jsx
@@ -1,13 +1,14 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
 import { BiLoaderCircle } from "react-icons/bi";
+import LoaderImg from "../../assets/loader.svg"
 import './Loader.css';
 
 const Loader = () => {
   return (
     <div className='loader flex flex-c'>
-      {/* <img src={LoaderImg} alt='loader' /> */}
-      <BiLoaderCircle />
+      <img src={LoaderImg} alt='loader' />
+      {/* <BiLoaderCircle /> */}
     </div>
   )
 }
--- Lib-Sys/src/pages/About/About.jsx
+++ Lib-Sys/src/pages/About/About.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import './About.css';
+import aboutImg from "../../assets/about-img.jpg"
 
 const About = () => {
   return (
-    <div>About</div>
+    <section className='about'>
+      <div className='container'>
+        <div className='section-title'>
+          <h2>About</h2>
+        </div>
+
+        <div className='about-content grid'>
+          <div className='about-img'>
+            <img src={aboutImg} alt='about img' />
+          </div>
+
+          <div className='about-text'>
+            <h2 className='about-title fs-26 ls-1'>About Books</h2>
+            <p className='fs-17'>Lorem ispurfjdkdhudhfdfhsfjsdfjhdfjhgfhdgfdhbnvbfdjgrggfhdbv</p>
+            <p className='fs-17'>xypz qbdfkdhfudfdvjfjdfhg</p>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
